90980e3669090d007732e73d950bb204ee965453
wqxu <2271548435@qq.com>
2016-10-05 12:22:54 +0800

    change down_era

--- down_era/setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cp .ecmwfapirc ~
-pip3 install https://software.ecmwf.int/wiki/download/attachments/56664858/ecmwf-api-client-python.tgz --user 
-pip install https://software.ecmwf.int/wiki/download/attachments/56664858/ecmwf-api-client-python.tgz --user 
---
+++ down_era/setup_api.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget https://bootstrap.pypa.io/get-pip.py
+python get-pip.py --user
+
+#pip3 install https://software.ecmwf.int/wiki/download/attachments/56664858/ecmwf-api-client-python.tgz --user 
+pip install https://software.ecmwf.int/wiki/download/attachments/56664858/ecmwf-api-client-python.tgz --user 
